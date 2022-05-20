fe4bdfe41ac77dfa05a61e9a25b5d5c56ce63b3e
Stefan Malanik <stefanmalanik@gmail.com>
2022-05-20 23:10:47 +0300

    Added bias loading behavior for gen script -- Observed biases.npy is in float format

--- testbench/codegen.py
+++ testbench/codegen.py
@@ -4,7 +4,7 @@ import os.path as path
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('i', type=str, nargs=1)
+parser.add_argument('i', type=str, nargs='+')
 parser.add_argument('-o', type=str, nargs=1)
 
 def get_array(arr, arr_name):
@@ -25,9 +25,13 @@ def get_array(arr, arr_name):
 def build_source(dict_of_arrays, file_name):
     ret = ''
     ret += f'#include "{file_name}.h"\n'
+    # Define arrays
     for name, val in dict_of_arrays.items():
         is_binary_flag = ((val == 0) | (val == 1)).all()
         ret += get_array(val, name + '_arr')
+    # Define matrices
+    for name, val in dict_of_arrays.items():
+        ret += f'matrix_t* {name}_mat;\n'
     # Create init mat function
     ret += 'void init_adj_mats(){\n'
     for name, val in dict_of_arrays.items():
@@ -41,26 +45,21 @@ def build_source(dict_of_arrays, file_name):
     ret += '}\n'
     return ret
 
-def build_header(dict_of_arrays, file_name):
-    ret = ''
-    ret += f'#ifndef {file_name.upper()}\n'
-    ret += f'#define {file_name.upper()}\n'
-    ret += '#include "matrix.h"\n'
-    for name in dict_of_arrays.keys():
-        ret += f'matrix_t* {name}_mat;\n'
-    ret += 'void init_adj_mats();\n'
-    ret += 'void free_adj_mats();\n'
-    ret += '#endif'
-    return ret
-
 def main():
     args = parser.parse_args()
-    with open(args.i[0], 'rb') as in_file, open(args.o[0] + '.c', 'w') as out_file_c, open(args.o[0] + '.h', 'w') as out_file_h:
-       data = np.load(in_file)
-       source_out = build_source(data, args.o[0])
-       out_file_c.write(source_out)
-       header_out = build_header(data, args.o[0])
-       out_file_h.write(header_out)
+    with open(args.o[0] + '.c', 'w') as out_file_c:
+        data = {}
+        for inp in args.i:
+            with open(inp, 'rb') as infile:
+                if path.splitext(inp)[1] == '.npz':
+                    data.update(np.load(infile).items())
+                elif path.splitext(inp)[1] == '.npy':
+                    data[path.basename(path.splitext(inp)[0])] = np.load(infile)
+                else:
+                    raise argparse.ArgumentError('Input files do not have proper format')
+
+        source_out = build_source(data, args.o[0])
+        out_file_c.write(source_out)
 
 main()
 
